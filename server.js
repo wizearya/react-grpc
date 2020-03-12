013d133c9bbbd634c7f01b14a993831c238c0fbe
wizearya <59855348+wizearya@users.noreply.github.com>
2020-03-12 12:17:41 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+var grpc = require('grpc');
+    var pingPongProto = grpc.load('./src/ping_pong.proto');
+    var server = new grpc.Server();
+
+
+    server.addService(pingPongProto.pingpong.PingPongService.service, {
+      pingPong: function(call,callback) {
+        console.log("Request")
+        return callback(null,{pong:"Pong"})
+      }
+    });
+
+
+    server.bind('localhost:8080',grpc.ServerCredentials.createInsecure());
+    server.start();
