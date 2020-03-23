f20a2fb66a874dd4a70763ea891780559a70d376
wizearya <arya@wize-net.co.jp>
2020-03-23 14:30:42 +0900

    update

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 const { PingPongServiceClient } = require('./ping_pong_grpc_web_pb');
-const { PingRequest, PongResponse } = require('./ping_pong_pb.js');
+const { PingRequest } = require('./ping_pong_pb.js');
 
-var client = new PingPongServiceClient('http://localhost:8080', null, null);
+var client = new PingPongServiceClient('http://localhost:9090', null, null);
 
 class App extends Component {
   
